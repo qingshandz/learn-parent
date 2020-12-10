20d956b3950c72321ed77f37111d7594cf305edd
wanghao <623455172@qq.com>
2020-12-10 16:39:29 +0800

    flyway示例

---
+++ lab-03-db-version/lab-03-db-version-flyway/src/main/java/com/cn/coder/lab03dbversionflyway/Lab03DbVersionFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.cn.coder.lab03dbversionflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab03DbVersionFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab03DbVersionFlywayApplication.class, args);
+    }
+
+}
---
+++ lab-03-db-version/lab-03-db-version-flyway/src/test/java/com/cn/coder/lab03dbversionflyway/Lab03DbVersionFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.coder.lab03dbversionflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab03DbVersionFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
