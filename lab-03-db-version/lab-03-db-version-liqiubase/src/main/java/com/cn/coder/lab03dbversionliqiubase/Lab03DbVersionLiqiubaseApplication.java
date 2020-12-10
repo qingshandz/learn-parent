b95abb033c9e47688dd617502e8796d54fbf4eb8
wanghao <623455172@qq.com>
2020-12-10 17:42:02 +0800

    liqiubase示例

---
+++ lab-03-db-version/lab-03-db-version-liqiubase/src/main/java/com/cn/coder/lab03dbversionliqiubase/Lab03DbVersionLiqiubaseApplication.java
@@ -0,0 +1,13 @@
+package com.cn.coder.lab03dbversionliqiubase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab03DbVersionLiqiubaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab03DbVersionLiqiubaseApplication.class, args);
+    }
+
+}
---
+++ lab-03-db-version/lab-03-db-version-liqiubase/src/test/java/com/cn/coder/lab03dbversionliqiubase/Lab03DbVersionLiqiubaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.coder.lab03dbversionliqiubase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab03DbVersionLiqiubaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
